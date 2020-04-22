a68e0160e4b1ad432c8550dcd9e88ad799c14db0
trainingid <meenkolambu64@gmail.com>
2020-04-22 12:10:47 +0530

    Web Project with package

---
+++ JenkinsWebProject/JenkinsWebProject/FirstForm.aspx.cs
@@ -0,0 +1,26 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JenkinsWebProject
+{
+    public partial class FirstForm : System.Web.UI.Page
+    {
+        //ILog log = log4net.LogManager.GetLogger(typeof(FirstForm)); FileAppender
+        ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Hello First Web Form!");
+          //  log4net.Config.XmlConfigurator.Configure();
+            
+            
+            log.Info("Test");
+            log.Debug("Test");
+            log.Error("Test");
+        }
+    }
+}
---
+++ JenkinsWebProject/JenkinsWebProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JenkinsWebProject.Global" Language="C#" %>
